0e58038a135dcdd1d4cb4027871a40b57a50be87
Mohammed Hamza <211220035@nitdelhi.ac.in>
2024-07-29 23:15:51 +0530

    fix

--- src/components/example/signup-form-demo.tsx
+++ src/components/example/signup-form-demo.tsx
@@ -71,7 +71,7 @@ export default function SignupFormDemo() {
     <div className="h-screen">
       <div className="max-w-md w-full mx-auto rounded-sm md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
         <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-          Welcome to Aceternity
+          Signup
         </h2>
 
         <form className="my-8" onSubmit={handleSubmit}>
